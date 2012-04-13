1145a35fba8f4e637bbe9cc067bc4c0e2b8a5b67
Carlos Rodriguez <carlos@s8f.org>
2012-04-12 23:29:39 -0700

    Split channels into own file.

--- tests/pubsub/client.js
+++ tests/pubsub/client.js
@@ -4,7 +4,7 @@ var context = require('rabbit.js').createContext();
 // Receive messages from server.js and relay to STDOUT.
 context.on('ready', function() {
   var c = new Channel(context, 'SUB', 'event::alphabet');
-  c.read(function(data) {
-    console.dir(data);
+  c.read(function(message) {
+    console.dir(message.data);
   });
 });
