8297780c42d6cc1e35110c0d9b927cfdeb3aeea4
Carlos Rodriguez <carlos@s8f.org>
2012-04-26 20:50:46 -0700

    Simplify example a bit.

--- examples/request/backend.js
+++ examples/request/backend.js
@@ -3,7 +3,7 @@ var agent = require('../../').init({debug: true})
   .use(require('agent-pubsub-redis'));
 
 agent.respond('backend', function(router, spec) {
-  router.get('/rand', function() {
+  router.get('/', function() {
     this.res.text("Your number is... \n\n" + Math.random() + "\n\nSincerely,\n" + spec.toString());
   });
   agent.log('listening on ' + spec.toString());
--- examples/request/frontend.js
+++ examples/request/frontend.js
@@ -6,16 +6,10 @@ var http = require('http')
   , server = http.createServer();
 
 server.on('request', function(req, res) {
-  if (req.url === '/') {
-    agent.request('agent://backend/rand').pipe(res);
-  }
-  else {
-    res.writeHead(404, {'content-type': 'text/plain; charset=utf-8'});
-    res.end('Page not found');
-  }
+  agent.request('agent://backend' + req.url).pipe(res);
 });
 
-var port = Math.floor(Math.random() * (50000 - 30000)) + 30000;
+var port = 3000;
 server.listen(port, function() {
   agent.log('Listening on http://localhost:' + port + '/');
 });
