e6376dcf5810d5ccc59003f610013d0d4c9ae99b
Carlos Rodriguez <carlos@s8f.org>
2012-05-07 12:57:42 -0700

    Add failover test

--- test/service.js
+++ test/service.js
@@ -18,6 +18,7 @@ function MockRequest(service) {
     self.socket.on('error', function(err) {
       self.emit('error', err);
     });
+    self.socket.setTimeout(100);
     self.socket.setEncoding('utf8');
   });
   amino.globalAgent.addRequest(this, service);
@@ -119,8 +120,8 @@ describe('service', function() {
 
       // Send 6 requests, server 3 should get all of them.
       for (var i = 0; i < 6; i++) {
-        (function(req) {
-          tasks.push(function(cb) {
+        tasks.push(function(cb) {
+          (function(req) {
             req.on('error', function(err) {
               assert.ifError(err);
             });
@@ -131,8 +132,8 @@ describe('service', function() {
               assert.strictEqual(data, 'hello:' + services[2].spec.id);
               cb(null, data);
             });
-          });
-        })(new MockRequest('test'));
+          })(new MockRequest('test'));
+        });
       }
       async.parallel(tasks, function(err, results) {
         assert.strictEqual(results.length, 6, 'all responses received');
@@ -140,6 +141,65 @@ describe('service', function() {
       });
     });
   });
-  it('should failover');
+  describe('failover', function() {
+    before(function(done) {
+      var tasks = [];
+      tasks.push(function(cb) {
+        var service = amino.createService('test@2.0.0');
+        service.once('spec', function(spec) {
+          service.server = net.createServer(function(socket) {
+            socket.on('data', function(data) {
+              socket.end(data.toString() + ':' + spec.id + ':2');
+            });
+          });
+          service.server.listen(spec.port, function() {
+            cb();
+          });
+        });
+      });
+      tasks.push(function(cb) {
+        services[2].server.on('close', function() {
+          cb();
+        });
+        services[2].server.close();
+      });
+      async.parallel(tasks, function() {
+        done();
+      });
+    });
+    // Failover should happen after the first failed request. Subsequent requests
+    // should use the updated spec list.
+    it('should failover', function(done) {
+      var tasks = [], hadError = false;
+      tasks.push(function(cb) {
+        var req = new MockRequest('test');
+        req.on('error', function(err) {
+          assert.strictEqual(err.code, 'ECONNREFUSED', 'server is down');
+          hadError = true;
+          cb();
+        });
+      });
+
+      for (var i = 0; i < 6; i++) {
+        tasks.push(function(cb) {
+          (function(req) {
+            req.on('error', function(err) {
+              assert.ifError(err);
+            });
+            req.on('connect', function() {
+              cb(null, 1);
+            });
+          })(new MockRequest('test'));
+        });
+      }
+
+      async.series(tasks, function(err, results) {
+        assert.ok(hadError, 'one error happened');
+        results.shift();
+        assert.strictEqual(results.length, 6, '6 responses came back');
+        done();
+      });
+    });
+  });
   it('should support versions');
 });
